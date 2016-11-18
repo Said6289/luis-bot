42b46c841c5c4307b3dbd92cae3d484f68647777
Said Al Attrach <said.alattrach@campus.tu-berlin.de>
2016-11-18 11:23:08 +0100

    Luisitiges commit

--- lbot.py
+++ lbot.py
@@ -1,1 +1,25 @@
-patterns = ['uis', 'lus', 'lys']
+#!python
+
+patterns = ["uis", "lus", "lys"]
+
+FILE_NAME = "wordlist.txt"
+NAME = "LUIS"
+
+def luisize(word):
+  words = []
+  for pat in patterns:
+    new_word = word.replace(pat, NAME)
+    if new_word != word:
+      words.append(new_word)
+  return words
+
+def main():
+  with open(FILE_NAME, "r") as f:
+    results = [line for line in f for substr in patterns if substr in line]
+
+  for r in results:
+    for a in luisize(r):
+      print(a)
+
+if __name__ == "__main__":
+  main()
